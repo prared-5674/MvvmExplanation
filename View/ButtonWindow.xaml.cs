c019c61dafbee35322f4e3d863fcdcd82646e891
prathapa <prathapa@golighthouse.com>
2024-04-14 01:05:54 -0700

    Add project files.

---
+++ View/ButtonWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MvvmTrail.View
+{
+    /// <summary>
+    /// Interaction logic for ButtonWindow.xaml
+    /// </summary>
+    public partial class ButtonWindow : UserControl
+    {
+        public ButtonWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModel/UIContent.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight.Command;
+using PropertyChanged;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MvvmTrail.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class UIContent
+    {
+        public string ButtonContent  = "Hello World!";
+        public ICommand ButtonCommand { get; set; }
+        
+        public UIContent() 
+        { 
+            ButtonCommand = new RelayCommand(OpenWindowAndSetString); 
+        }
+
+        public void OpenWindowAndSetString()
+        {
+            MessageBox.Show(ButtonContent);
+        }
+
+        
+    }
+}
